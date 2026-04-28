f094d7739b182722a29fecf67f7555af109a7b12
VijalDsouza <vijal.datagrokr.co>
2026-04-28 12:07:05 +0530

    chore: add dbt target/, logs/, .user.yml to .gitignore; remove tracked generated files

--- dbt_project/target/run/cc_transactions_lake/models/silver/silver_accounts.sql
+++
@@ -1,3 +0,0 @@
-create or replace view "dbt_catalog"."main"."silver_accounts__dbt_int" as (
-        select * from 'D:/Credit_Card_Lake/data/silver/accounts/data.parquet'
-    );
--- dbt_project/target/run/cc_transactions_lake/models/silver/silver_transaction_codes.sql
+++
@@ -1,3 +0,0 @@
-create or replace view "dbt_catalog"."main"."silver_transaction_codes__dbt_int" as (
-        select * from 'D:/Credit_Card_Lake/data/silver/transaction_codes/data.parquet'
-    );
